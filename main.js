040ba15e5c8eb2b55e32c07ab1054089204ef42c
perborgen <perhb@hotmail.com>
2015-08-10 13:24:58 +0200

    main.js chnges

--- main.js
+++ main.js
@@ -1,85 +1,9 @@
-var TodoList = React.createClass({
-	render: function(){
-		
-		var todos = this.props.todos.map(function(todo,index){
-			return <li key={index}>
-						<span 
-							onClick={this.props.handleRemove.bind(null, index)}>
-							X
-						</span>
-						{todo}
-					</li>
-		}.bind(this));
-
-		return (
-			<ul>
-				{todos}
-			</ul>
-		);
-	}
-});
-
-var InputField = React.createClass({
-	render: function(){
-		return(
-			<div>
-				<input 
-					type="text"
-					value={this.props.text}
-					onChange={this.props.handleChange} />
-				<input 
-					type="submit"
-					value="Add to list!" 
-					onClick={this.props.handleClick} />
-			</div>
-		);
-	}
-});
+var TODOS = ["buy food", "make food", "eat food"];
 
 var TodoApp = React.createClass({
-	getInitialState: function(){
-		return {
-			text: '',
-			todos: []
-		}
-	},
-
-	handleChange: function(event){
-		this.setState({
-			text: event.target.value
-		});
-	},
-
-	handleClick: function(){
-		this.setState({
-			todos: this.state.todos.concat(this.state.text),
-			text: ''
-		});
-	},
-
-	handleRemove: function(index){
-		newTodos = this.state.todos;
-		newTodos.splice(index,1);
-		this.setState({
-			todos: newTodos
-		});
-	},
-
-	render: function(){
-		return (
-			<div>
-				<h1>Todo app</h1>
-				<InputField 
-					text={this.state.text} 
-					handleClick={this.handleClick} 
-					handleChange={this.handleChange} />
-				<TodoList 
-					todos={this.state.todos}
-					handleRemove={this.handleRemove} />
-			</div>
-		);
-	}
+    render: function(){
+        return(<div>Todo app: {this.props.todos}</div>);
+    },
 });
 
-React.render(<TodoApp />,  document.getElementById("content"));
-
+React.render(<TodoApp todos={TODOS} />, document.getElementById('container'));
