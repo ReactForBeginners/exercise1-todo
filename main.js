f233c0110c0b92a85e4a73c14d8ae7f1df73f489
perborgen <perhb@hotmail.com>
2016-01-12 21:17:13 +0100

    Add js to html file

--- main.js
+++
@@ -1,9 +0,0 @@
-var TODOS = ["buy food", "make food", "eat food"];
-
-var TodoApp = React.createClass({
-    render: function(){
-        return(<div>{this.props.todos}</div>);
-    },
-});
-
-React.render(<TodoApp todos={TODOS} />, document.getElementById('content'));
